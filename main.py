3235f0e9c0acb563f6f9dd354a0bbb9f3478c248
Sugeng Priyanto <spriyanto360@gmail.com>
2021-02-24 09:00:26 +0000

    version 1.0

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 print("Welcome to Band Name Generator")
 city = input("What city did your band formed?\n")
+pet = input("What is your pet name?\n")
+leader = input("What is the name of the band leader?\n")
+noun = input("Type a singular noun in your mind right now\n")
+tm = input("What is your favourite Day, Month, or Season?\n")
+col = input("What is your favourite color?\n")
+body = input("Type a part of human body\n")
+
+name1 = city + " " + pet
+name2 = noun + " " + leader
+name3 = col + " " + tm
+name4 = col + " " + body
+name5 = city + " " + tm
+name6 = col + " " + leader
+name7 = noun + " " + pet
+name8 = col + " " + pet
+
+name = [name1, name2, name3, name4, name5, name6, name7, name8]
+
+for i in name:
+  print(i)
